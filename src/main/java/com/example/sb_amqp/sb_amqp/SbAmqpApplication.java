824367e687d276d062078bb05368cbd6193593c9
lengyuetao <dyadan@163.com>
2018-05-02 09:11:36 +0800

    Initial commit

---
+++ src/main/java/com/example/sb_amqp/sb_amqp/SbAmqpApplication.java
@@ -0,0 +1,12 @@
+package com.example.sb_amqp.sb_amqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAmqpApplication.class, args);
+	}
+}
